d16fbba6059070169b2baeb11947d79c5d289720
Vaibhav Yenamandra <3663231+envp@users.noreply.github.com>
2022-07-22 23:51:02 -0400

    Rework DVD bounce so it changes color upon bounce

--- src/DVDBounce/DVDBounce.cpp
+++ src/DVDBounce/DVDBounce.cpp
@@ -1,13 +1,38 @@
 #include "SFML/Graphics.hpp"
+#include "SFML/Graphics/Color.hpp"
 #include "SFML/Graphics/RectangleShape.hpp"
 #include "SFML/Graphics/Sprite.hpp"
 #include "SFML/Graphics/Texture.hpp"
 #include "SFML/Window/VideoMode.hpp"
 #include <filesystem>
+#include <iterator>
+#include <random>
 
 #include "Assets.h"
 
+constexpr sf::Color ORANGE = sf::Color(/* red   = */ 0xff,
+                                       /* green = */ 0x7f,
+                                       /* blue  = */ 0x00);
+
+// clang-format off
+constexpr sf::Color ALL_COLORS[] = {
+    sf::Color::Red,
+    ORANGE,
+    sf::Color::Yellow,
+    sf::Color::Green,
+    sf::Color::Blue,
+    sf::Color::Cyan,
+    sf::Color::Magenta,
+    sf::Color::White
+};
+// clang-format on
+
+constexpr size_t NUM_COLORS = std::size(ALL_COLORS);
+
 int main() {
+  std::default_random_engine Generator;
+  std::uniform_int_distribution<size_t> RandomIndex(0, NUM_COLORS - 1);
+
   constexpr static sf::Vector2u SCREEN_SIZE{800, 600};
   sf::RenderWindow Window(sf::VideoMode(SCREEN_SIZE), "DVD");
   Window.setFramerateLimit(60);
@@ -15,7 +40,7 @@ int main() {
   std::filesystem::path AssetRoot(DVDBOUNCE_ASSET_ROOT);
 
   sf::Texture DVDLogo;
-  if (!DVDLogo.loadFromFile(AssetRoot / "dvd-logo.png")) {
+  if (!DVDLogo.loadFromFile(AssetRoot / "dvd-logo-texture.png")) {
     Window.close();
     return -1;
   }
@@ -24,7 +49,7 @@ int main() {
   DVDSprite.setTexture(DVDLogo);
   DVDSprite.setColor(sf::Color(0, 255, 0));
   const auto DIMENSIONS = DVDLogo.getSize();
-  sf::Vector2f Velocity{1, 1};
+  sf::Vector2f Velocity{2, 2};
 
   while (Window.isOpen()) {
     sf::Event Event;
@@ -36,19 +61,27 @@ int main() {
       }
     }
 
-    Window.clear(sf::Color::White);
+    Window.clear();
     const auto &TopLeft = DVDSprite.getPosition();
-
+    bool Bounced = false;
     // Invert X-velocity if it touches the vertical bounds anywhere
     if (TopLeft.x < 0 || TopLeft.x + static_cast<float>(DIMENSIONS.x) >=
                              static_cast<float>(SCREEN_SIZE.x)) {
       Velocity.x *= -1;
+      Bounced = true;
     }
 
     // Invert Y-velocity if it touches the horizontal bounds anywhere
     if (TopLeft.y < 0 || TopLeft.y + static_cast<float>(DIMENSIONS.y) >=
                              static_cast<float>(SCREEN_SIZE.y)) {
       Velocity.y *= -1;
+      Bounced = true;
+    }
+
+    if (Bounced) {
+      size_t Index = RandomIndex(Generator);
+      assert(Index < NUM_COLORS && "Index out of bounds!");
+      DVDSprite.setColor(ALL_COLORS[1]);
     }
 
     DVDSprite.setPosition(TopLeft + Velocity);
