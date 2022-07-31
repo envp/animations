d6a471e108bcb81473c4482ac7b2dc35338c0aba
Vaibhav Yenamandra <3663231+envp@users.noreply.github.com>
2022-07-30 23:28:06 -0400

    Improve Starfield animation
    - Spawn 'new' stars in a uniform distribution instead of a normal
      - They now spawn all over the viewport. Previous it appeared as if they
         were being emitted from around the center
    - Draw stars as a white circle with a fixed size outline
      - The outline is affected by scale as well.
    - Increase outward velocity, and scaling.
      - Use a larger constant for more non-linearity in later stages.

--- src/Starfield/Starfield.cpp
+++ src/Starfield/Starfield.cpp
@@ -26,16 +26,14 @@ struct PhaseCoordinate {
 
 class WorldState {
 private:
-  using FloatDist = std::normal_distribution<float>;
+  using FloatDist = std::uniform_real_distribution<float>;
   using UIntDist = std::uniform_int_distribution<size_t>;
   WorldState() = delete;
 
 public:
   WorldState(std::pair<float, float> XBounds, std::pair<float, float> YBounds)
-      : XDist(new FloatDist((XBounds.second + XBounds.first) / 2,
-                            (XBounds.second - XBounds.first) / 4)),
-        YDist(new FloatDist((YBounds.second + YBounds.first) / 2,
-                            (YBounds.second - YBounds.first) / 4)) {}
+      : XDist(new FloatDist(XBounds.first, XBounds.second)),
+        YDist(new FloatDist(YBounds.first, YBounds.second)) {}
 
   std::pair<float, float> getRandomXY() {
     float X = (*XDist)(Engine);
@@ -47,7 +45,9 @@ public:
     // Some values are repeated to
     constexpr sf::Color ALLOWED_STAR_COLORS[] = {
         // Reddish
-        sf::Color(0xCF, 0x51, 0x40),
+        sf::Color(0xCF, 0x7F, 0x40),
+        // Yellow
+        sf::Color(0xF8, 0xEE, 0xA5),
         // Powder Blue
         sf::Color(0xB0, 0xE0, 0xE6),
         // Orange-ish
@@ -56,11 +56,6 @@ public:
     };
     static UIntDist ColorDist{0, std::size(ALLOWED_STAR_COLORS)};
     return ALLOWED_STAR_COLORS[ColorDist(Engine)];
-  }
-
-  void reset() {
-    XDist->reset();
-    YDist->reset();
   }
 
 private:
@@ -75,20 +70,23 @@ int main() {
   sf::RenderWindow Window(sf::VideoMode(ScreenSize), "starfield");
   Window.setFramerateLimit(60);
 
-  // Tiny factor which we scale with by repeaated multiplication.
+  // Tiny factor which we scale with by repeated multiplication.
   // Because it is so small, the scaling *looks* approximately linear over the
-  constexpr float SCALE_FACTOR = 1.006;
+  // course of a animation, speeding up towards the end.
+  constexpr float SCALE_FACTOR = 1.01;
   WorldState State({0, static_cast<float>(WINDOW_WIDTH) - 1},
                    {0, static_cast<float>(WINDOW_HEIGHT) - 1});
 
+  sf::CircleShape DefaultStar(/* radius = */ 1.0f);
+  DefaultStar.setFillColor(sf::Color::White);
+  DefaultStar.setOutlineThickness(0.5f);
   sf::CircleShape StarShapes[NUM_STARS];
-  std::fill_n(std::begin(StarShapes), NUM_STARS,
-              sf::CircleShape(/* radius = */ 1.0f));
+  std::fill_n(std::begin(StarShapes), NUM_STARS, DefaultStar);
   float StarScale[NUM_STARS];
   std::fill_n(std::begin(StarScale), NUM_STARS, 1.0f);
 
   for (auto &Star : StarShapes) {
-    Star.setFillColor(State.getRandomColor());
+    Star.setOutlineColor(State.getRandomColor());
   }
   PhaseCoordinate Coordinates[NUM_STARS];
 
@@ -96,16 +94,26 @@ int main() {
   for (auto &Coordinate : Coordinates) {
     const auto &P = State.getRandomXY();
     sf::Vector2f Position(P.first, P.second);
-    sf::Vector2f Velocity = (Position - Origin).normalized().cwiseMul({2, 2});
+    sf::Vector2f Velocity = (Position - Origin).normalized().cwiseMul({4, 4});
     Coordinate = {.Position = Position, .Velocity = Velocity};
   }
+
+  bool Paused = false;
 
   while (Window.isOpen()) {
     sf::Event Event;
     while (Window.pollEvent(Event)) {
       if (Event.type == sf::Event::Closed) {
         Window.close();
+      } else if (Event.type == sf::Event::MouseButtonPressed) {
+        Paused = true;
+      } else if (Event.type == sf::Event::MouseButtonReleased) {
+        Paused = false;
       }
+    }
+
+    if (Paused) {
+      continue;
     }
 
     // BEGIN FRAME
@@ -131,7 +139,7 @@ int main() {
       } else {
         auto NextPos = State.getRandomXY();
         auto NextColor = State.getRandomColor();
-        Star.setFillColor(NextColor);
+        Star.setOutlineColor(NextColor);
         Coord.Position.x = NextPos.first;
         Coord.Position.y = NextPos.second;
         Coord.Velocity =
